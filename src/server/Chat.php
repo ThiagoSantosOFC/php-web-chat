e46c08d319aa98f97aa6adfb2b4e5b822e0b9905
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-06-01 14:55:21 +0100

    a

---
+++ src/server/Chat.php
@@ -0,0 +1,37 @@
+<?php
+namespace MyApp;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+        echo "Nova conexão! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($client !== $from) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+        echo "Conexão fechada! ({$conn->resourceId})\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "Ocorreu um erro: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+}
